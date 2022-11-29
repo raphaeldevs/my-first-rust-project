4557c1893c1292b62879637451d175530336955f
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2022-11-28 21:55:46 -0300

    chore: add first dependency with Cargo from Crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use rfnd_hello_world::HELLO_WORLD;
+
 fn main() {
     println!("Hello, Rapha!");
+    println!(
+        "I installed my first package with Cargo from Crates: {}.",
+        HELLO_WORLD
+    );
 }
