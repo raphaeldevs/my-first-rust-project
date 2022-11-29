3b4158390139c6773dd34a8d695dc3de5ea8395a
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2022-11-28 22:33:50 -0300

    feat: reading file using low-level way

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
-use rfnd_hello_world::HELLO_WORLD;
+use std::fs::File;
+use std::io::Read;
+use std::str::from_utf8;
 
 fn main() {
-    println!("Hello, Rapha!");
-    println!(
-        "I installed my first package with Cargo from Crates: {}.",
-        HELLO_WORLD
-    );
+    let file_path = "hello.txt";
+
+    let mut file = match File::open(file_path) {
+        Err(err) => panic!("Couldn't read file: {}", err.to_string()),
+        Ok(value) => value,
+    };
+
+    let stat = match file.metadata() {
+        Err(err) => panic!("Couldn't get stat: {}", err.to_string()),
+        Ok(value) => value,
+    };
+
+    let mut buffer = vec![0; stat.len() as usize];
+
+    match file.read(&mut buffer) {
+        Err(err) => panic!("Couldn't read: {}", err.to_string()),
+        Ok(_) => (),
+    };
+
+    let data = match from_utf8(&buffer) {
+        Err(err) => panic!("Couldn't convert buffer to string: {}", err.to_string()),
+        Ok(value) => value,
+    };
+
+    print!("{}", data)
 }
